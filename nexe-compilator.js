60902010aa5fdd39445d41ffe51d71e258edd9e7
Michael Rojas <michael.rojasp@gmail.com>
2016-12-05 16:07:37 -0600

    Copy key_manager into dist folder.

--- nexe-compilator.js
+++ nexe-compilator.js
@@ -1,15 +1,24 @@
 var nexe = require('nexe');
+var ncp = require('ncp');
 
 nexe.compile({
   input: 'index.js', // where the input file is
-  output: 'nexe^$', // where to output the compiled binary
+  output: 'dist/asana-exporter/asana-exporter.exe', // where to output the compiled binary
   nodeVersion: '6.3.0', // node version
-  nodeTempDir: 'dist', // where to store node source.
+  nodeTempDir: 'temp', // where to store node source.
   nodeConfigureArgs: ['opt', 'val'], // for all your configure arg needs.
   framework: "node", // node, nodejs, or iojs
-  browserifyExcludes: ['try-thread-sleep']
+  browserifyExcludes: ['try-thread-sleep','ncp','nexe']
 }, function(err) {
   if(err) {
     return console.log(err);
+  }else{
+    ncp.limit = 8;
+    ncp('key_manager','dist/asana-exporter/key_manager',function(ncpError){
+      if(ncpError){
+        return console.error(ncpError);
+      }
+      console.log('Distributable generated successfully.');
+    });
   }
 });
